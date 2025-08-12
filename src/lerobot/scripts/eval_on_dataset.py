a899d14427d361bf40ed9c7dbc1fc22826c2f24f
Zhenghao Ke <imkzh@qq.com>
2025-08-12 09:24:22 +0800

    feature: evaluation on dataset.

---
+++ src/lerobot/scripts/eval_on_dataset.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import logging
+import time
+from contextlib import nullcontext
+from pprint import pformat
+from typing import Any
+from pathlib import Path
+
+import torch
+from termcolor import colored
+from torch.amp import GradScaler
+from torch.optim import Optimizer
+
+from lerobot.constants import ACTION
+from lerobot.configs import parser
+from lerobot.configs.train import TrainPipelineConfig
+from lerobot.datasets.factory import make_dataset
+from lerobot.datasets.sampler import EpisodeAwareSampler
+from lerobot.datasets.utils import cycle
+from lerobot.envs.factory import make_env
+from lerobot.optim.factory import make_optimizer_and_scheduler
+from lerobot.policies.factory import make_policy
+from lerobot.policies.pretrained import PreTrainedPolicy
+from lerobot.policies.utils import get_device_from_parameters
+from lerobot.scripts.eval import eval_policy
+from lerobot.utils.logging_utils import AverageMeter, MetricsTracker
+from lerobot.utils.random_utils import set_seed
+from lerobot.utils.train_utils import (
+    get_step_checkpoint_dir,
+    get_step_identifier,
+    load_training_state,
+    save_checkpoint,
+    update_last_checkpoint,
+)
+from lerobot.utils.utils import (
+    format_big_number,
+    get_safe_torch_device,
+    has_method,
+    init_logging,
+)
+from lerobot.utils.wandb_utils import WandBLogger
+from torch.utils.tensorboard import SummaryWriter
+
+
+def run_policy(
+    train_metrics: MetricsTracker,
+    policy: PreTrainedPolicy,
+    batch: Any,
+    # optimizer: Optimizer,
+    # grad_clip_norm: float,
+    # grad_scaler: GradScaler,
+    # lr_scheduler=None,
+    # use_amp: bool = False,
+    # lock=None,
+) -> tuple[MetricsTracker, dict]:
+    start_time = time.perf_counter()
+    # device = get_device_from_parameters(policy)
+    policy.eval()
+    with nullcontext():
+        loss, loss_dict = policy.forward(batch)
+        # TODO(rcadene): policy.unnormalize_outputs(out_dict)
+
+    train_metrics.loss = loss.item()
+    # train_metrics.lr = optimizer.param_groups[0]["lr"]
+    train_metrics.update_s = time.perf_counter() - start_time
+    return train_metrics, loss_dict
+
+
+def guess_output_path(cfg):
+    if cfg.output_dir is not None:
+        return cfg.output_dir
+    
+    policy_path = parser.get_path_arg("policy")
+    if policy_path is not None:
+        return Path(policy_path) / "eval"
+
+    return None
+
+
+@parser.wrap()
+def eval(cfg: TrainPipelineConfig):
+    cfg.output_dir = guess_output_path(cfg)
+    cfg.validate()
+    logging.info(pformat(cfg.to_dict()))
+
+    tb_logger = None
+    wandb_logger = None
+    if cfg.wandb.enable and cfg.wandb.project:
+        wandb_logger = WandBLogger(cfg)
+
+    if cfg.tensorboard.enable:
+        tb_logger = SummaryWriter(cfg.output_dir / "journal")
+
+    if wandb_logger is None and tb_logger is None:
+        logging.info(colored("Logs will be saved locally.", "yellow", attrs=["bold"]))
+
+    if cfg.seed is not None:
+        set_seed(cfg.seed)
+
+    # Check device is available
+    device = get_safe_torch_device(cfg.policy.device, log=True)
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cuda.matmul.allow_tf32 = True
+
+    logging.info("Creating dataset")
+    dataset = make_dataset(cfg)
+
+    # Create environment used for evaluating checkpoints during training on simulation data.
+    # On real-world data, no need to create an environment as evaluations are done outside train.py,
+    # using the eval.py instead, with gym_dora environment and dora-rs.
+    eval_env = None
+    if cfg.eval_freq > 0 and cfg.env is not None:
+        logging.info("Creating env")
+        eval_env = make_env(cfg.env, n_envs=cfg.eval.batch_size, use_async_envs=cfg.eval.use_async_envs)
+
+    logging.info("Creating policy")
+    policy = make_policy(
+        cfg=cfg.policy,
+        ds_meta=dataset.meta,
+    )
+
+    # logging.info("Creating optimizer and scheduler")
+    # optimizer, lr_scheduler = make_optimizer_and_scheduler(cfg, policy)
+    # grad_scaler = GradScaler(device.type, enabled=cfg.policy.use_amp)
+
+    step = 0  # number of policy updates (forward + backward + optim)
+
+    # if cfg.resume:
+    #     step, optimizer, lr_scheduler = load_training_state(cfg.checkpoint_path, optimizer, lr_scheduler)
+
+    num_learnable_params = sum(p.numel() for p in policy.parameters() if p.requires_grad)
+    num_total_params = sum(p.numel() for p in policy.parameters())
+
+    logging.info(colored("Output dir:", "yellow", attrs=["bold"]) + f" {cfg.output_dir}")
+    if cfg.env is not None:
+        logging.info(f"{cfg.env.task=}")
+    logging.info(f"{cfg.steps=} ({format_big_number(cfg.steps)})")
+    logging.info(f"{dataset.num_frames=} ({format_big_number(dataset.num_frames)})")
+    logging.info(f"{dataset.num_episodes=}")
+    logging.info(f"{num_learnable_params=} ({format_big_number(num_learnable_params)})")
+    logging.info(f"{num_total_params=} ({format_big_number(num_total_params)})")
+
+    # create dataloader for offline training
+    sampler = EpisodeAwareSampler(
+        dataset.episode_data_index,
+        drop_n_last_frames=1,
+        shuffle=False,
+    )
+    shuffle = False
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        num_workers=cfg.num_workers,
+        batch_size=cfg.batch_size,
+        shuffle=shuffle,
+        sampler=sampler,
+        pin_memory=device.type == "cuda",
+        drop_last=False,
+    )
+
+    dl_iter = iter(dataloader)
+
+    policy.eval()
+
+    train_metrics = {
+        "loss": AverageMeter("loss", ":.3f"),
+        "grad_norm": AverageMeter("grdn", ":.3f"),
+        "lr": AverageMeter("lr", ":0.1e"),
+        "update_s": AverageMeter("updt_s", ":.3f"),
+        "dataloading_s": AverageMeter("data_s", ":.3f"),
+    }
+
+    train_tracker = MetricsTracker(
+        cfg.batch_size, dataset.num_frames, dataset.num_episodes, train_metrics, initial_step=step
+    )
+
+    logging.info("Start offline evaluation on a fixed dataset")
+    for batch in dl_iter:
+        start_time = time.perf_counter()
+        train_tracker.dataloading_s = time.perf_counter() - start_time
+
+        for key in batch:
+            if isinstance(batch[key], torch.Tensor):
+                batch[key] = batch[key].to(device, non_blocking=device.type == "cuda")
+
+        train_tracker, output_dict = run_policy(
+            train_tracker,
+            policy,
+            batch,
+            # optimizer,
+            # cfg.optimizer.grad_clip_norm,
+            # grad_scaler=grad_scaler,
+            # lr_scheduler=lr_scheduler,
+            # use_amp=cfg.policy.use_amp,
+        )
+
+        # Note: eval and checkpoint happens *after* the `step`th training update has completed, so we
+        # increment `step` here.
+        step += 1
+        train_tracker.step()
+        is_log_step = cfg.log_freq > 0 and step % cfg.log_freq == 0
+        # is_saving_step = step % cfg.save_freq == 0 or step == cfg.steps
+        # is_eval_step = cfg.eval_freq > 0 and step % cfg.eval_freq == 0
+
+        if is_log_step:
+            logging.info(train_tracker)
+            tracker_stats = train_tracker.to_dict() 
+
+            if wandb_logger:
+                wandb_logger.log_dict(tracker_stats, step)
+                wandb_logger.log_dict(output_dict, step)
+
+            if tb_logger:
+                for scalar_name in output_dict:
+                    tb_logger.add_scalar(f"sample.{scalar_name}", output_dict[scalar_name], step)
+
+                for scalar_name in tracker_stats:
+                    tb_logger.add_scalar(f"eval_tracker.{scalar_name}", tracker_stats[scalar_name], step)
+
+            # train_tracker.reset_averages()
+
+        # if cfg.save_checkpoint and is_saving_step:
+        #     logging.info(f"Checkpoint policy after step {step}")
+        #     checkpoint_dir = get_step_checkpoint_dir(cfg.output_dir, cfg.steps, step)
+        #     save_checkpoint(checkpoint_dir, step, cfg, policy, optimizer, lr_scheduler)
+        #     update_last_checkpoint(checkpoint_dir)
+        #     if wandb_logger:
+        #         wandb_logger.log_policy(checkpoint_dir)
+
+    logging.info("End of evaluation")
+
+    # if cfg.policy.push_to_hub:
+    #     policy.push_model_to_hub(cfg)
+
+
+def main():
+    init_logging()
+    eval()
+
+
+if __name__ == "__main__":
+    main()
